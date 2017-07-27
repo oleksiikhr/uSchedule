6bfae2adcb081f4931bba1afc43e6a0aee0993b1
Alexey Khrusch <alexeykhr@outlook.com>
2017-07-27 11:28:49 +0300

    Update edit template

--- resources/assets/js/views/schedule/Edit.vue
+++ resources/assets/js/views/schedule/Edit.vue
@@ -1,11 +1,12 @@
 <template>
-    <md-layout class="container edit-template">
+    <md-layout class="edit-template">
         <md-theme md-name="block">
-            <md-layout class="back-main">
+            <md-layout class="left-column">
                 <div class="subjects phone-viewport">
                     <md-tabs md-fixed>
                         <md-tab md-icon="book">
                             <md-list class="md-dense">
+                                <!--TODO: Skip block-->
                                 <draggable :list="subjects" :clone="clone" :options="{group:{ name:'days', pull:'clone', put:false }}">
                                     <md-list-item class="subject-block" v-for="(element, index) in subjects" :key="index">
                                         <span>{{element.title}}</span>
@@ -15,100 +16,39 @@
                         </md-tab>
 
                         <md-tab md-icon="people">
-                            <p style="color:#fff;">Преподователь1</p>
-                            <p style="color:#fff;">Преподователь2</p>
+                            <p style="color: rgba(255, 255, 255, .87);">Преподователь1</p>
+                            <p style="color: rgba(255, 255, 255, .87);">Преподователь2</p>
                         </md-tab>
                     </md-tabs>
                 </div>
             </md-layout>
         </md-theme>
 
-        <md-layout md-flex="70" class="right-column">
+        <md-layout md-flex="75" class="right-column">
             <div class="schedule">
+
                 <div class="schedule-column">
                     <h3>Понеділок</h3>
+
+                    <span>Пара</span>
+                    <span>Предмет</span>
+                    <span>Викладач</span>
+                    <span>Видалення</span>
+
+                    <!--TODO: if empty => not working-->
                     <draggable :list="day1" class="dragArea" :options="{group:'days'}">
                         <div v-for="(element, index) in day1" :key="index" class="subject-block list-subject-item">
-                            <div class="lesson-number md-align-center md-vertical-align-center">{{index + 1}}</div>
-                            <div class="subject-name md-align-center md-vertical-align-center">
-                                <div class="md-flex-90">
-                                    {{ element.title }}
-                                </div>
-                                <div class="md-flex-10" @click="removeSubject(day1, index)">
-                                    <md-icon class="text-danger">delete</md-icon>
-                                </div>
-                            </div>
+                            {{index + 1}}
+                            {{ element.title }}
+                            <a @click="removeSubject(day1, index)">
+                                <md-icon class="text-danger">delete</md-icon>
+                            </a>
                         </div>
                     </draggable>
+
                 </div>
 
-                <div class="schedule-column">
-                    <h3>Вівторок</h3>
-                    <draggable :list="day2" class="dragArea md-flex-100" :options="{group:'days'}">
-                        <div v-for="(element, index) in day2" :key="index" class="subject-block list-subject-item">
-                            <div class="lesson-number md-align-center md-vertical-align-center">{{index + 1}}</div>
-                            <div class="subject-name md-align-center md-vertical-align-center">
-                                <div class="md-flex-90">
-                                    {{ element.title }}
-                                </div>
-                                <div class="md-flex-10" @click="removeSubject(day2, index)">
-                                    <md-icon class="text-danger">delete</md-icon>
-                                </div>
-                            </div>
-                        </div>
-                    </draggable>
-                </div>
-
-                <div class="schedule-column">
-                    <h3>Середа</h3>
-                    <draggable :list="day3" class="dragArea md-flex-100" :options="{group:'days'}">
-                        <div v-for="(element, index) in day3" :key="index" class="subject-block list-subject-item">
-                            <div class="lesson-number md-align-center md-vertical-align-center">{{index + 1}}</div>
-                            <div class="subject-name md-align-center md-vertical-align-center">
-                                <div class="md-flex-90">
-                                    {{ element.title }}
-                                </div>
-                                <div class="md-flex-10" @click="removeSubject(day3, index)">
-                                    <md-icon class="text-danger">delete</md-icon>
-                                </div>
-                            </div>
-                        </div>
-                    </draggable>
-                </div>
-
-                <div class="schedule-column">
-                    <h3>Четверг</h3>
-                    <draggable :list="day4" class="dragArea md-flex-100" :options="{group:'days'}">
-                        <div v-for="(element, index) in day4" :key="index" class="subject-block list-subject-item">
-                            <div class="lesson-number md-align-center md-vertical-align-center">{{index + 1}}</div>
-                            <div class="subject-name md-align-center md-vertical-align-center">
-                                <div class="md-flex-90">
-                                    {{ element.title }}
-                                </div>
-                                <div class="md-flex-10" @click="removeSubject(day4, index)">
-                                    <md-icon class="text-danger">delete</md-icon>
-                                </div>
-                            </div>
-                        </div>
-                    </draggable>
-                </div>
-
-                <div class="schedule-column">
-                    <h3>Пятниця</h3>
-                    <draggable :list="day5" class="dragArea md-flex-100" :options="{group:'days'}">
-                        <div v-for="(element, index) in day5" :key="index" class="subject-block list-subject-item">
-                            <div class="lesson-number md-align-center md-vertical-align-center">{{index + 1}}</div>
-                            <div class="subject-name md-align-center md-vertical-align-center">
-                                <div class="md-flex-90">
-                                    {{ element.title }}
-                                </div>
-                                <div class="md-flex-10" @click="removeSubject(day5, index)">
-                                    <md-icon class="text-danger">delete</md-icon>
-                                </div>
-                            </div>
-                        </div>
-                    </draggable>
-                </div>
+                <!--TODO list -> day2, day3, day4, day5, day6-->
             </div>
         </md-layout>
     </md-layout>
@@ -116,9 +56,11 @@
 
 <script>
     import draggable from 'vuedraggable'
+    import MdList from "../../../../../node_modules/vue-material/src/components/mdList/mdList.vue";
 
     export default{
         components: {
+            MdList,
             draggable,
         },
 
@@ -130,34 +72,6 @@
             return {
                 subjects: [],
                 day1: [{
-                    title: "Об'єктно-орієнтоване програмування"
-                }, {
-                    title: "Операційні системи"
-                }, {
-                    title: "Веб-дизайн"
-                }],
-                day2: [{
-                    title: "Об'єктно-орієнтоване програмування"
-                }, {
-                    title: "Операційні системи"
-                }, {
-                    title: "Веб-дизайн"
-                }],
-                day3: [{
-                    title: "Об'єктно-орієнтоване програмування"
-                }, {
-                    title: "Операційні системи"
-                }, {
-                    title: "Веб-дизайн"
-                }],
-                day4: [{
-                    title: "Об'єктно-орієнтоване програмування"
-                }, {
-                    title: "Операційні системи"
-                }, {
-                    title: "Веб-дизайн"
-                }],
-                day5: [{
                     title: "Об'єктно-орієнтоване програмування"
                 }, {
                     title: "Операційні системи"
