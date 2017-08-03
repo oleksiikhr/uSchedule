134348f6d8bc41c1c8781aba60de2ebc075bd42b
Alexey Khrusch <alexeykhr@outlook.com>
2017-08-03 13:37:35 +0300

    Add live search in Edit.vue, fix style

--- resources/assets/js/views/schedule/Edit.vue
+++ resources/assets/js/views/schedule/Edit.vue
@@ -1,28 +1,35 @@
 <template>
     <md-layout class="edit-template">
-        <!--<md-theme md-name="block">-->
-            <md-layout md-flex="25" class="left-column">
-                <div class="subjects phone-viewport">
-                    <md-tabs md-fixed>
-                        <md-tab md-icon="book">
-                            <md-list class="md-dense no-padding">
-                                <!--TODO: Skip block-->
-                                <draggable :list="subjects" :clone="clone" :options="{group:{ name:'days', pull:'clone', put:false }}">
-                                    <md-list-item class="subject-block" v-for="(element, index) in subjects" :key="index">
-                                        <span :title="element.title">{{ element.title }}</span>
-                                    </md-list-item>
-                                </draggable>
-                            </md-list>
-                        </md-tab>
+        <md-layout md-flex="25" class="left-column">
+            <div class="subjects phone-viewport">
+                <md-tabs md-fixed>
+                    <md-tab md-icon="book">
+                        <md-list class="md-dense no-padding">
+                            <form novalidate @submit.stop.prevent="submit">
+                                <md-input-container>
+                                    <label>Пошук предмету</label>
+                                    <md-input v-model="searchSubject"></md-input>
+                                </md-input-container>
+                            </form>
+                            <!--TODO: Add empty block-->
+                            <draggable :list="filterSubjects(subjects, searchSubject)" :clone="clone"
+                                       :options="{group:{ name:'days', pull:'clone', put:false }}">
+                                <md-list-item :title="element.title" class="subject-block"
+                                              v-for="(element, index) in filterSubjects(subjects, searchSubject)" :key="element.id">
+                                    <span>{{ element.title }}</span>
+                                </md-list-item>
+                            </draggable>
+                        </md-list>
+                    </md-tab>
 
-                        <md-tab md-icon="people">
-                            <p style="color: #333;">Преподователь1</p>
-                            <p style="color: #333">Преподователь2</p>
-                        </md-tab>
-                    </md-tabs>
-                </div>
-            </md-layout>
-        <!--</md-theme>-->
+                    <md-tab md-icon="people">
+                        <!--TODO: add draggable-->
+                        <p style="color: #333;">Преподователь1</p>
+                        <p style="color: #333">Преподователь2</p>
+                    </md-tab>
+                </md-tabs>
+            </div>
+        </md-layout>
 
         <md-layout md-flex="75" class="right-column">
             <div class="schedule">
@@ -31,14 +38,13 @@
                     <h3>Понеділок</h3>
                     <draggable :list="day1" class="dragArea" :options="{group:'days'}">
                         <div v-for="(element, index) in day1" :key="index" class="subject-block list-subject-item">
-                            {{index + 1}}
+                            {{ index + 1 }}
                             {{ element.title }}
                             <a @click="removeSubject(day1, index)">
                                 <md-icon class="text-danger">delete</md-icon>
                             </a>
                         </div>
                     </draggable>
-
                 </div>
 
                 <!--TODO list -> day2, day3, day4, day5, day6-->
@@ -50,9 +56,7 @@
 <script>
     import draggable from 'vuedraggable'
 
-//    TODO: http://leafo.net/sticky-kit/
-
-    export default{
+    export default {
         components: {
             draggable,
         },
@@ -64,6 +68,8 @@
         data() {
             return {
                 subjects: [],
+                teachers: [],
+                searchSubject: null,
                 day1: [{
                     title: "Об'єктно-орієнтоване програмування"
                 }, {
@@ -75,6 +81,8 @@
         },
 
         mounted() {
+//            TODO: /api/teachers.get
+
             axios.get('/api/subjects.get', {
                 params: {
                     faculty: this.faculty,
@@ -82,31 +90,49 @@
                 }
             })
                 .then(response => this.subjects = response.data)
-                .catch(error => console.log('Error:' + this.error));
+                .catch(error => console.log('Error: ' + this.error));
         },
 
         methods: {
-            click: function(item) {
+            click(item) {
                 item.name="IT GETS CLONED";
             },
-            add: function() {
+            add() {
                 this.list.push({
                     name: 'Juan'
                 });
             },
-            clone: function(el) {
+            clone(el) {
                 return {
                     title: el.title
                 };
             },
-            replace: function() {
+            replace() {
                 this.list = [{
                     name: 'Edgard'
                 }]
             },
-            removeSubject: function(list, index) {
+            removeSubject(list, index) {
                 list.splice(index, 1);
-            }
-        }
+            },
+            filterSubjects(items, find) {
+                let arr = items,
+                    search = find;
+
+                if (! search) {
+                    return arr;
+                }
+
+                search = search.toLowerCase();
+
+                arr = arr.filter(function(item) {
+                    if (item.title.toLowerCase().indexOf(search) !== -1) {
+                        return item;
+                    }
+                });
+
+                return arr;
+            },
+        },
     }
 </script>
