d4dcead25620b77f5d9df320df2efb7fe7a5d4b8
niksib <kolyasub@ukr.net>
2017-09-06 22:27:10 +0300

    add laravel voyager

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ Route::get('/', 'DashboardController@index');
 Route::post('/dashboard/find', 'DashboardController@find');
 
 Route::resource('schedule', 'ScheduleController');
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
