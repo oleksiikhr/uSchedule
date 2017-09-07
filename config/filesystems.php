4a22a0918e8ef4cad8200683a8785cdacc670b15
niksib <kolyasub@ukr.net>
2017-09-07 19:02:17 +0300

    fix save profile image

--- config/filesystems.php
+++ config/filesystems.php
@@ -45,13 +45,13 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root' => storage_path('app/public'),
         ],
 
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => '/storage',
+            'url' => '/storage/public',
             'visibility' => 'public',
         ],
 
