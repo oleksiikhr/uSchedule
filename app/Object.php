0e4b4e779420aa21684328ad017ce2df6e06a156
Nikolay Syabrenko <kolyasub@ukr.net>
2017-12-07 12:51:56 +0200

    add slugable extension

--- app/Object.php
+++ app/Object.php
@@ -2,9 +2,31 @@
 
 namespace App;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 
 class Object extends Model
 {
+    use Sluggable;
+
     protected $table = 'objects';
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+
+    public function objectType()
+    {
+        return $this->hasOne('App\ObjectType', 'type_id');
+    }
 }
--- app/ObjectType.php
+++ app/ObjectType.php
@@ -2,9 +2,31 @@
 
 namespace App;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 
 class ObjectType extends Model
 {
+    use Sluggable;
+
     protected $table = 'objects_types';
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+
+    public function objects()
+    {
+        return $this->hasMany('App\Object', 'type_id', 'id');
+    }
 }
