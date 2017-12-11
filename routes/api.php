cea295fcd4bb16b522426f1dc2c86653f6139884
Alexey Khrusch <alexeykhr@outlook.com>
2017-12-11 11:05:13 +0200

    Updated docs

--- routes/api.php
+++ routes/api.php
@@ -1,38 +1,42 @@
 <?php
 
-/*
- * User is not logged in.
+/* | -------------------------------------------------------------------
+ * | User is not logged in.
+ * | -------------------------------------------------------------------
  */
 Route::group(['middleware' => 'guest'], function () {
 
-    Route::post('/register', 'AuthController@register'); // TODO: make (email, password, password_repeat, object_id)
-    Route::post('/login', 'AuthController@login');
-    Route::post('/restore-password', 'AuthController@restorePassword'); // TODO: write this method
-    Route::get('/refresh-token', 'AuthController@refreshToken');
+    /** @see \App\Http\Controllers\AuthController - Auth Section */
+    Route::post('register', 'AuthController@register'); // TODO: make (email, password, object_id)
+    Route::post('login', 'AuthController@login');
+    Route::post('restore-password', 'AuthController@restorePassword'); // TODO: write this method
+    Route::get('refresh-token', 'AuthController@refreshToken');
 
 });
 
-/*
- * User authorized.
+/* | -------------------------------------------------------------------
+ * | User authorized.
+ * | -------------------------------------------------------------------
  */
 Route::group(['middleware' => 'jwt.auth'], function () {
 
-    Route::post('/logout', 'AuthController@logout');
+    Route::post('logout', 'AuthController@logout');
 
-    // Profile
+    /** @see \App\Http\Controllers\ProfileController - Profile Section */
     Route::group(['prefix' => 'profile'], function () {
         Route::get('/', 'ProfileController@index');
     });
 
-    // Object
+    /** @see \App\Http\Controllers\ObjectController - Object Section */
     Route::group(['prefix' => 'object'], function () {
         Route::get('{id}', 'ObjectController@one');
     });
 
 });
 
-/*
- * Public.
+/* | -------------------------------------------------------------------
+ * | Public methods.
+ * | -------------------------------------------------------------------
  */
 Route::get('objects', 'ObjectController@list');
 Route::get('schedules', 'ScheduleController@one');
