3f47fbcea57ba03db3e53f58522aca0aa364166e
Nikolay Syabrenko <kolyasub@ukr.net>
2017-12-11 14:34:48 +0200

    change DB config for heroku

--- config/database.php
+++ config/database.php
@@ -56,11 +56,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('HEROKU_DB_HOST', '127.0.0.1'),
+            'port' => env('HEROKU_DB_PORT', '5432'),
+            'database' => env('HEROKU_DB_DATABSE', 'forge'),
+            'username' => env('HEROKU_DB_USERNAME', 'forge'),
+            'password' => env('HEROKU_DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
