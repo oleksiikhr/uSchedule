bdd58d8a80a63dc644ebdd9a179e352c5e211ea0
niksib <kolyasub@ukr.net>
2018-04-26 13:14:44 +0300

    add register route

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use Illuminate\Http\Request;
 
 Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
     Route::post('login', 'AuthController@login');
+    Route::post('register', 'AuthController@register');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
