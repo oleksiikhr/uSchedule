66fcd75db79f1449bc7c4608dc9e6c0b6606a02e
Alexey Khrusch <alexeykhr@outlook.com>
2018-09-01 21:31:53 +0300

    Add teacher_subject table

---
+++ database/migrations/2018_09_01_183006_create_teacher_subject_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeacherSubjectTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teacher_subject', function (Blueprint $table) {
+            $table->unsignedInteger('teacher_id');
+            $table->unsignedInteger('subject_id');
+
+            $table->foreign('teacher_id')->references('id')->on('teachers')->onDelete('cascade');
+            $table->foreign('subject_id')->references('id')->on('subjects')->onDelete('cascade');
+
+            $table->primary(['teacher_id', 'subject_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teacher_subject');
+    }
+}
