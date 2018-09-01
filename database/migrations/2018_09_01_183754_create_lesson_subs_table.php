4414c7d404d831a0e9ce032e779146b406284caf
Alexey Khrusch <alexeykhr@outlook.com>
2018-09-01 21:40:17 +0300

    Add lesson_subs table

---
+++ database/migrations/2018_09_01_183754_create_lesson_subs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLessonSubsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lesson_subs', function (Blueprint $table) {
+            $table->unsignedInteger('teacher_id');
+            $table->unsignedInteger('lesson_id');
+            $table->unsignedTinyInteger('type_id');
+            $table->string('cabinet')->nullable();
+
+            $table->foreign('teacher_id')->references('id')->on('teachers')->onDelete('cascade');
+            $table->foreign('lesson_id')->references('id')->on('lessons')->onDelete('cascade');
+            $table->foreign('type_id')->references('id')->on('lesson_types')->onDelete('cascade');
+
+            $table->primary(['teacher_id', 'lesson_id', 'type']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lesson_subs');
+    }
+}
