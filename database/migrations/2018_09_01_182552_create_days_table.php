705d20e7dabb48026428e647e7f3876432e22f79
Alexey Khrusch <alexeykhr@outlook.com>
2018-09-01 21:27:22 +0300

    Add days table

---
+++ database/migrations/2018_09_01_182552_create_days_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('days', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('column_id');
+            $table->tinyInteger('order');
+            $table->timestamps();
+
+            $table->foreign('column_id')->references('id')->on('columns')->onDelete('cascade');
+
+            $table->index('column_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('days');
+    }
+}
