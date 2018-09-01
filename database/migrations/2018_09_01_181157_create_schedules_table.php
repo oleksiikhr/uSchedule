7b8021da39eb4204d2ad863d25f923b1ed011feb
Alexey Khrusch <alexeykhr@outlook.com>
2018-09-01 21:15:00 +0300

    Add schedules table

---
+++ database/migrations/2018_09_01_181157_create_schedules_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('faculty_id');
+            $table->unsignedTinyInteger('degree');
+            $table->unsignedTinyInteger('course');
+            $table->unsignedTinyInteger('daytime');
+            $table->timestamps();
+
+            $table->foreign('faculty_id')->references('id')->on('faculties')->onDelete('cascade');
+
+            $table->index('faculty_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
